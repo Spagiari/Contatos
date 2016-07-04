f1a17e230d62e3de9375c654f155336d2a206439
ios5948 <ios5948@mac-mini-de-caelum-65.local>
2016-07-04 09:49:59 -0300

    Initial Commit

---
+++ ContatosIP67/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContatosIP67
+//
+//  Created by ios5948 on 04/07/16.
+//  Copyright © 2016 Caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
